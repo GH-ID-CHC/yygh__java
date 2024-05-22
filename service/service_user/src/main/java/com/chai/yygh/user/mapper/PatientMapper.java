3ad45e01ae594332bdababc91779f2c7c3469f62
柴和程 <344785479@qq.com>
2024-05-22 22:03:55 +0800

    feat: 用户端
    [就诊人管理]

---
+++ service/service_user/src/main/java/com/chai/yygh/user/mapper/PatientMapper.java
@@ -0,0 +1,16 @@
+package com.chai.yygh.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chai.yygh.model.user.Patient;
+
+import java.util.List;
+
+/**
+ * @program: yygh
+ * @author:
+ * @create: 2024-05-21 22:19
+ */
+public interface PatientMapper extends BaseMapper<Patient> {
+
+}
